da77fc5a4134efcdc90c29637e398c7cf33e5d5d
DKNorad <nedd.petrov@gmail.com>
2023-03-27 02:13:28 +0300

    file handler for monitoring

---
+++ file_handler.py
@@ -0,0 +1,28 @@
+import os.path
+from logging_setup import logger
+from time import sleep, time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+
+class ModifiedFileHandler(PatternMatchingEventHandler):
+    last_modified = 0
+
+    def on_modified(self, event):
+        super(ModifiedFileHandler, self).on_modified(event)
+        current_time = time()
+        if (current_time - self.last_modified) > 0.1:
+            logger.info(f"File {event.src_path} was just modified")
+            self.last_modified = current_time
+
+
+class FileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.event_handler = ModifiedFileHandler(patterns=[self.file_path])
+        self.watched_dir = os.path.split(self.file_path)[0]
+        self.observer = Observer()
+
+    def run(self):
+        self.observer.schedule(self.event_handler, self.watched_dir, recursive=False)
+        self.observer.start()
